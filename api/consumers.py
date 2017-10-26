93279022e8890be01cd4c4c805ae8bf319dd58e8
Nghia Nguyen <dannguyen@paypal.com>
2017-10-26 20:24:04 +0800

    add confirm ws message

--- api/consumers.py
+++ api/consumers.py
@@ -4,6 +4,7 @@ def ws_connect(message, user):
 	print("User group connected: ", user)
 	Group(user).add(message.reply_channel)
 	message.reply_channel.send({"accept": True})
+	Group(user).send({'text': '{"message":"' + user + ' is connected."}'}, immediately=True)
 	if user == 'cam':
 		Group('staff').send({'text': '{"message":"cam is connected"}'}, immediately=True)
 	if user == 'staff':
